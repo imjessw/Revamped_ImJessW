cf96ddcfcb250d85dd2800ca8dfec307e3068c2b
Jessica Waite <imjessw@gmail.com>
2014-05-05 14:46:22 -0400

    updated css working on responsiveness   added app.'s

--- app.js
+++ app.js
@@ -8,6 +8,21 @@ var routes = require('./routes');
 var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
+var email= require ('emailjs/email');
+var server= email.server.connect({
+<<<<<<< HEAD
+	user: "imjessw@gmail.com",
+	password: "",//password
+	host: "smtp.gmail.com",
+=======
+	user: "app19024585@heroku.com",
+	password: "wpecniz9",
+	host: "smtp.sendgrid.net",
+>>>>>>> 43ade99bcde6b89af06c7ac275be132b35956496
+	ssl: true
+
+})
+
 
 var app = express();
 
@@ -22,6 +37,7 @@ app.use(express.methodOverride());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
+
 // development only
 if ('development' == app.get('env')) {
   app.use(express.errorHandler());
@@ -29,6 +45,25 @@ if ('development' == app.get('env')) {
 
 app.get('/', routes.index);
 app.get('/users', user.list);
+app.post('/sendEmail', function(req, res){
+	var appData=req.body;
+	console.log(appData)
+	console.log(appData.first+" "+ appData.lastName+" "+ appData.email+" "+appData.message);
+	res.send("hello")
+	
+	server.send({
+		text: appData.first+" "+ appData.lastName+" "+ appData.email+" "+appData.message, // this will become my input
+		from: "Jess <app19024585@heroku.com>",
+		to: "Jess <imjessw@gmail.com>",
+		subject: "I\'m JessW Site Intro"
+	}, function(err, message){
+		console.log(err);
+		console.log(message);
+	})
+
+})
+
+
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
